8079f4371796a40e7ec34040faa6a3f5fabaa173
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2020-11-16 05:51:20 +0700

    *Create expanding lists with List(,children:)

---
+++ NewInIOS14_SwiftUI/NewInIOS14_SwiftUI/Views/ExpandingLisstView.swift
@@ -0,0 +1,41 @@
+//
+//  ExpandingLisstView.swift
+//  NewInIOS14_SwiftUI
+//
+//  Created by Trinh Thai on 11/16/20.
+//
+
+import SwiftUI
+
+
+struct Bookmark: Identifiable {
+    let id = UUID()
+    let name: String
+    let icon: String
+    var items: [Bookmark]?
+    
+    static let apple = Bookmark(name: "Apple", icon: "1.circle")
+    static let bbc = Bookmark(name: "BBC", icon: "square.and.pencil")
+    static let swift = Bookmark(name: "Swift", icon: "bolt.fill")
+    static let twitter = Bookmark(name: "Twitter", icon: "mic")
+    
+    static let example1 = Bookmark(name: "Favorites", icon: "star", items: [Bookmark.apple, Bookmark.bbc, Bookmark.swift, Bookmark.twitter])
+    static let example2 = Bookmark(name: "Recent", icon: "timer", items: [Bookmark.apple, Bookmark.bbc, Bookmark.swift, Bookmark.twitter])
+    static let example3 = Bookmark(name: "Recommended", icon: "hand.thumbsup", items: [Bookmark.apple, Bookmark.bbc, Bookmark.swift, Bookmark.twitter])
+}
+
+struct ExpandingLisstView: View {
+    let items: [Bookmark] = [.example1, .example2, .example3]
+    var body: some View {
+        List(items, children: \.items) { row in
+            Image(systemName: row.icon)
+            Text(row.name)
+        }
+    }
+}
+
+struct ExpandingLisstView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpandingLisstView()
+    }
+}
