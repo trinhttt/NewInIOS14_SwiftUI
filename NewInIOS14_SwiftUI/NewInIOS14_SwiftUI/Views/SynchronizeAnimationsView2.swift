14ca1250a74fd189115d713233f7563eb48a6f9a
Thai Thi Tu Trinh <ttttrinh132@gmail.com>
2020-11-20 06:39:36 +0700

    *expanding a small view up to something larger when tapped

--- NewInIOS14_SwiftUI/NewInIOS14_SwiftUI/Views/SynchronizeAnimationsView2.swift
+++ NewInIOS14_SwiftUI/NewInIOS14_SwiftUI/Views/SynchronizeAnimationsView2.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct SynchronizeAnimationsView2: View {
+    @Namespace private var animation
+    @State private var isZoomed = false
+    
+    var frame: CGFloat {
+        isZoomed ? 300 : 44
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+            
+            VStack {
+                HStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.blue)
+                        .frame(width: frame, height: frame)
+                        .padding(.top, isZoomed ? 20 : 0)
+                    
+                    if isZoomed == false {
+                        Text("Taylor Swift – 1989")
+                            .matchedGeometryEffect(id: "AlbumTitle", in: animation)
+                            .font(.headline)
+                        Spacer()
+                    }
+                }
+                
+                if isZoomed == true {
+                    Text("Taylor Swift – 1989")
+                        .matchedGeometryEffect(id: "AlbumTitle", in: animation)
+                        .font(.headline)
+                        .padding(.bottom, 60)
+                    Spacer()
+                }
+            }
+            .onTapGesture {
+                withAnimation(.spring()) {
+                    self.isZoomed.toggle()
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .frame(height: isZoomed ? 400 : 60)
+            .background(Color(white: 0.9))
+        }
     }
 }
 
